7b0d8fad79b37d6e946ad72a56ac032c19b4f55b
Kris Savoia <kris@linktr.ee>
2022-01-07 12:53:33 +1100

    split grid & carousel components

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -3,15 +3,17 @@ import { Box } from "@linktr.ee/ui-link-kit";
 
 import { GridItem } from "../GridItem";
 
-export const Grid = ({ imageSrcs }: { imageSrcs?: string[] }) => {
+export const Carousel = ({ imageSrcs }: { imageSrcs?: string[] }) => {
   const length = imageSrcs?.filter(Boolean)?.length || 0;
 
   return (
     <Box
       display="grid"
       gridGap={10}
-      gridTemplateColumns={["repeat(3, 1fr)"]}
-      gridTemplateRows={[`repeat(${length > 3 ? 2 : 1}, 200px)`]}
+      gridTemplateColumns={[
+        `repeat(${length}, calc(100% ${length > 1 ? "- 40px" : ""}))`,
+      ]}
+      gridTemplateRows={["minmax(150px, 1fr)"]}
       overflow="auto"
       paddingBottom={[32, 0]}
     >
